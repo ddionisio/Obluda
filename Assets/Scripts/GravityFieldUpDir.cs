fdc2f18fb35ffc7818a1c759415c79e3685d14fe
David Dionisio <garapata@gmail.com>
2013-07-17 14:26:29 -0700

    moved physics stuff to MateUnity, can be used for other projects

--- Assets/Scripts/GravityFieldUpDir.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class GravityFieldUpDir : GravityFieldBase {
-
-    protected override Vector3 GetUpVector(Vector3 position) {
-        return transform.up;
-    }
-}
